1b59c52b5ae53d5cfc82ae09948fa086af37ae33
Andrew Craig <andy.peter.craig@gmail.com>
2021-01-02 17:07:26 +0900

    Change type_check() to return lintr-style result

--- R/interface.R
+++ R/interface.R
@@ -12,6 +12,19 @@ interpreter <- function() {
 }
 
 
+#' @param x An XML tree returned by `xml2::as_list(xml2::read_xml())`
+get_line_col <- function(x) {
+    if (!is.list(x[[1]])) {
+        return(list(
+            line_number = as.integer(attr(x, "line1")),
+            column_number = as.integer(attr(x, "col1"))
+        ))
+    } else {
+        return(Recall(x[[1]]))
+    }
+}
+
+
 #' Type check an R file
 #'
 #' @param path A character string; the file path.
@@ -22,24 +35,36 @@ interpreter <- function() {
 #' type_check(file)
 #'
 #' @export
-type_check <- function(path) {
-    es <- rlang::parse_exprs(file(path))
+type_check <- function(text) {
+    es <- parse(text = text, keep.source = TRUE)
+    tree <- xml2::as_list(xml2::read_xml(xmlparsedata::xml_parse_data(es)))[[1]]
     envir <- list()
     safe_eval_type <- purrr::safely(eval_type)
-    for (expr in es) {
+    errors <- list()
+    for (i_expr in seq_along(es)) {
+        expr <- es[[i_expr]]
         res <- safe_eval_type(expr, envir)
         if (!is.null(res$error)) {
+            line_col <- get_line_col(tree[[i_expr]])
             cat("In the expression:\n")
-            print(expr)
             cat("The following type error is found:\n")
             cat(res$error$message)
-            return(invisible(NULL))
+            errors <- c(errors, list(list(
+                filename = "<text>",
+                line_number = line_col$line_number,
+                column_number = line_col$column_number,
+                type = "type",
+                message = res$error$message,
+                line = expr,
+                ranges = NULL,
+                linter = "typeChecker"
+            )))
         } else {
             envir <- res$result$envir
         }
     }
     cat("The file is type-checked.")
-    invisible(NULL)
+    errors
 }
 
 push <- function(xs, el) {
